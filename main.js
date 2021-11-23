40afd2e14cec90779888884ff9f450a3384078de
bradley <bradleybkempton@gmail.com>
2021-11-23 08:11:13 -0700

    Added html content for grid

---
+++ main.js
@@ -0,0 +1,25 @@
+
+
+
+//operate function
+function operate (x, operator, y){
+//make switch for opperator execute?
+
+    //math operators
+    function mult(a, b) {
+        return a * b;
+    }
+
+    function div(a, b) {
+        return a / b;
+    }
+
+    function add(a, b) {
+        return a + b;
+    }
+
+    function sub(a, b) {
+        return a - b;
+    }
+
+}//end operate
