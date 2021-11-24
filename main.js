8979d8998daddf6fcc7da85cd7d7a9dadb41952b
bradley <bradleybkempton@gmail.com>
2021-11-23 20:45:36 -0700

    Added DOM manipulation for number inputs in JS

--- main.js
+++ main.js
@@ -1,3 +1,65 @@
+const screen = document.getElementById("screen");
+screen.textContent = 0;
+
+let a = 0;
+let b = 0;
+
+
+
+
+
+//button functions
+const one = document.querySelector('#one');
+one.addEventListener('click', () => {
+    screen.textContent = 1;
+
+});
+
+const two = document.querySelector('#two');
+two.addEventListener('click', () => {
+    screen.textContent = 2;
+});
+
+const three = document.querySelector('#three');
+three.addEventListener('click', () => {
+    screen.textContent = 3;
+});
+
+const four = document.querySelector('#four');
+four.addEventListener('click', () => {
+    screen.textContent = 4;
+});
+
+const five = document.querySelector('#five');
+five.addEventListener('click', () => {
+    screen.textContent = 5;
+});
+
+const six = document.querySelector('#six');
+six.addEventListener('click', () => {
+    screen.textContent = 6;
+});
+
+const seven = document.querySelector('#seven');
+seven.addEventListener('click', () => {
+    screen.textContent = 7;
+});
+
+const eight = document.querySelector('#eight');
+eight.addEventListener('click', () => {
+    screen.textContent = 8;
+});
+
+const nine = document.querySelector('#nine');
+nine.addEventListener('click', () => {
+    screen.textContent = 9;
+});
+
+const clear = document.querySelector('#clear');
+clear.addEventListener('click', () => {
+    screen.textContent = 0;
+});
+
 
 
 
@@ -23,3 +85,4 @@ function operate (x, operator, y){
     }
 
 }//end operate
+
