a048b5bdb02b695fd9d8ea28f023a022e574d00b
bradley <bradleybkempton@gmail.com>
2021-11-24 09:11:48 -0700

    Added math operators in main.js. Altered eventListener btns in html.

--- main.js
+++ main.js
@@ -1,71 +1,160 @@
 const screen = document.getElementById("screen");
 screen.textContent = 0;
 
-let a = 0;
-let b = 0;
-
-
-
-
+let a;
+let b;
+let operator;
+let num;
+let digit;
+let dotBool = true;
+let placeNumBool = true;
+let answer;
 
 //button functions
 const one = document.querySelector('#one');
 one.addEventListener('click', () => {
-    screen.textContent = 1;
-
+    digit = "1";
+    store(digit);
+    screen.textContent = num;
 });
 
 const two = document.querySelector('#two');
 two.addEventListener('click', () => {
-    screen.textContent = 2;
+    digit = "2";
+    store(digit);
+    screen.textContent = num;
 });
 
 const three = document.querySelector('#three');
 three.addEventListener('click', () => {
-    screen.textContent = 3;
+    digit = "3";
+    store(digit);
+    screen.textContent = num;
 });
 
 const four = document.querySelector('#four');
 four.addEventListener('click', () => {
-    screen.textContent = 4;
+    digit = "4";
+    store(digit);
+    screen.textContent = num;
 });
 
 const five = document.querySelector('#five');
 five.addEventListener('click', () => {
-    screen.textContent = 5;
+    digit = "5";
+    store(digit);
+    screen.textContent = num;
 });
 
 const six = document.querySelector('#six');
 six.addEventListener('click', () => {
-    screen.textContent = 6;
+    digit = "6";
+    store(digit);
+    screen.textContent = num;
 });
 
 const seven = document.querySelector('#seven');
 seven.addEventListener('click', () => {
-    screen.textContent = 7;
+    digit = "7";
+    store(digit);
+    screen.textContent = num;
 });
 
 const eight = document.querySelector('#eight');
 eight.addEventListener('click', () => {
-    screen.textContent = 8;
+    digit = "8";
+    store(digit);
+    screen.textContent = num;
 });
 
 const nine = document.querySelector('#nine');
 nine.addEventListener('click', () => {
-    screen.textContent = 9;
+    digit = "9";
+    store(digit);
+    screen.textContent = num;
+});
+
+const zero = document.querySelector('#zero');
+zero.addEventListener('click', () => {
+    if (num != undefined) {
+        digit = "0";
+        store(digit);
+        screen.textContent = num;
+    }
+});
+
+const dot = document.querySelector('#dot');
+dot.addEventListener('click', () => {
+    if (num != undefined) {
+        if (Boolean(dotBool)) {
+            digit = ".";
+            store(digit);
+            screen.textContent = num;
+            dotBool = false;
+        }
+    }
 });
 
 const clear = document.querySelector('#clear');
 clear.addEventListener('click', () => {
     screen.textContent = 0;
+    a = undefined;
+    b = undefined;
+    operator = undefined;
+    num = undefined;
+    digit = undefined;
+    dotBool = true;
+    answer = undefined;
+});
+
+const times = document.querySelector('#times');
+times.addEventListener('click', () => {
+    placeNum();
+    operator = "*";
+    console.log(operator);
+    if (Boolean(placeNumBool)){
+        answer = parseInt(a) * parseInt(b);
+        screen.textContent = answer;
+    }
+    
+});
+
+const divide = document.querySelector('#divide');
+divide.addEventListener('click', () => {
+    placeNum();
+    operator = "/";
+    console.log(operator);
+});
+
+const plus = document.querySelector('#plus');
+plus.addEventListener('click', () => {
+    placeNum();
+    operator = "+";
+    console.log(operator);
+});
+
+
+const minus = document.querySelector('#minus');
+minus.addEventListener('click', () => {
+    placeNum();
+    operator = "-";
+    console.log(operator);
+});
+
+const equals = document.querySelector('#equals');
+equals.addEventListener('click', () => {
+    console.log("a: " + a);
+    console.log("b: " + b);
+    console.log(placeNumBool);
 });
 
 
 
 
+
 //operate function
-function operate (x, operator, y){
-//make switch for opperator execute?
+function operate() {
+    //make switch for opperator execute?
 
     //math operators
     function mult(a, b) {
@@ -86,3 +175,24 @@ function operate (x, operator, y){
 
 }//end operate
 
+
+function store(digit) {
+    if (num == undefined) {
+        return num = digit;
+
+    } else {
+        return num += digit;
+    }
+}
+
+function placeNum(){
+    if(a == undefined){
+    a = num;
+    num = undefined;
+    screen.textContent = 0;
+    placeNumBool = false;
+    }else if (b == undefined){
+        b = num;
+        placeNumBool = true;
+    }
+}
